123cf291fc0576b9679001bc44ca36df24568d99
mainic <nicolas.maire@unibas.ch>
2016-04-01 11:41:18 +0200

    fixed id bug, simplified code

--- WHO_INTERVA4_MAPPING.R
+++ WHO_INTERVA4_MAPPING.R
@@ -8,14 +8,12 @@
 ## @output:		csv for usage in InterVA4
 ## @author: 	RMI
 ## ----------------------------------------------------------------------
-
+#Clear variables
+rm(list=ls(all=TRUE))
 library(InterVA4)
 library(foreach)
 
 cat("\nWHO VA Instrument 2014 -> InterVA4 Conversion\n\n")
-
-#Clear variables
-rm(list=ls(all=TRUE))
 
 ## Define your file path variables here###############################
 workingDir = file.path(getwd(), "data")
@@ -38,12 +36,6 @@ loadAndSetAllVariablesFromWHOInstrument<-function(entryLevel){
 		header = names(who)[j]
 		value =  as.character(entry[1,j])
 		header_cleaned = regmatches(header, regexpr("[^\\.]*$", header))
-
-		#Set value to 0 if NA to prevent NA when evaluating expression
-		#Check how to replace this, since this introduces some errors (e.g. value < 10)
-		if(is.na(value)){
-			value = -1;
-		}
 		assign(header_cleaned, value, envir = .GlobalEnv) # put variables in global environment
 	}
 }
@@ -53,42 +45,27 @@ mapping = read.csv2(mappingFileName)
 
 #Run through mappings file and fill in value for every InterVA4 variable
 who_n = nrow(mapping)
-#who_n = 1 # Limit to first x entries for testing purposes
 counter = 1
 
-outputData <- data.frame(matrix(ncol=246)) #Initialize output dataframe
+outputData <- data.frame(matrix(ncol=who_n+1)) #Initialize output dataframe
+colnames(outputData) <- c("ID", toupper(mapping[, 2]))
 
-rows <- foreach(entryCount=1:entries ) %do%{	
+rows <- foreach(entryCount=1:entries ) %do%{
 	loadAndSetAllVariablesFromWHOInstrument(entryCount)
 	#Prepare output
-	currentData <- data.frame(matrix(ncol=246))
+	currentData <- data.frame(matrix(ncol=who_n+1))
+	currentData[1] = entryCount
 	x <- foreach(i=1:who_n) %do%{	
-		##Assign who variable
-		#who_var = as.character(mapping[i,4]) # Convert to class character from factors
 		expression = as.character(mapping[i,5])
 		interva = as.character(mapping[i, 2])
 		id = mapping[i, 1]
-		question = mapping[i,3]
-		#Set Column names and also ID
-		if(i == 1){
-			colnames(currentData)[1] <- "ID"
-			currentData[1] = i
-		}
-		colnames(currentData)[i+1] <- interva
-		if(class(interva) == "character" && nchar(interva) > 0){
-			##Evaluate expression and set InterVA4 variable accordingly
-			retVal = eval(parse(text=expression))
-			if(retVal == TRUE){
-				##Evaluate expression and set InterVA4 variable accordingly
-				currentData[i+1] = 'y'
-			}
-		}else{
-			print("Empty")
+		##Evaluate expression and set InterVA4 variable accordingly
+		retVal = eval(parse(text=expression))
+		if(retVal == TRUE){
+			currentData[i+1] = 'y'
 		}
 		counter = counter + 1
 	}
-	colnames(currentData) <- toupper(colnames(currentData)) #Change colnames to all uppercase
-	colnames(outputData) <- toupper(colnames(currentData)) #Set column names for output
 	outputData[entryCount,] <- currentData
 }
 write.table(outputData, outputFileName, quote=FALSE, row.names = FALSE, na="", qmethod = "escape", sep = ",")
